46dfd02653cad2b8215d959a53ad905bf44b8329
fasyanbls <fasya.salim@student.president.ac.id>
2025-08-20 22:22:49 +0700

    Fix base URL for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/landing-page-course/', // 👈 ini penting biar path assetnya bener
 })
